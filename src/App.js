497600d26443d729b0373289ea86a8464187947a
Jaron-Lane <jaronwohlwend@icloud.com>
2021-12-03 14:55:24 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        
+        <Route path="/:pageName" element={<ErrorPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+function Login() {
+  return (
+    <div>
+      <h3>Login Page</h3>
+      <button>Login here</button>
+    </div>
+  )
+};
+
+function Signup() {
+  return (
+    <div>
+      <h3>Signup Page</h3>
+      <button>Signup here</button>
+    </div>
+  )
+};
+
+function About() {
+  return (
+    <div>
+      <h3>About Page</h3>
+      <button>About</button>
+    </div>
+  )
+};
+
+function Logout() {
+  return (
+    <div>
+      <h3>Logout Page</h3>
+      <button>Logout here</button>
+    </div>
+  )
+};
+
+function ErrorPage() {
+  const params = useParams()
+
+  return (
+    <div>
+      <h3>ERROR: {params.pageName} PAGE NOT FOUND</h3>
+    </div>
+  )
 }
 
 export default App;
